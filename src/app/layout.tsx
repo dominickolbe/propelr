399d2b6a9b6961628454e612cac02e838b97a4f0
Dominic Kolbe <kolbedominic@gmail.com>
2024-02-07 07:51:22 +0000

    feat: Add Ackee tracking script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://hello.domnc.app/hello.js"
+        data-ackee-server="https://hello.domnc.app"
+        data-ackee-domain-id="1f4776fa-4767-49c8-a111-38f3c3f5bfcd"
+      ></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
